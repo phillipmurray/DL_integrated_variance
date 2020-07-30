a2b09990a1d88e7e33db1c7368d80c593a28a184
phillipmurray <37044444+phillipmurray@users.noreply.github.com>
2020-07-30 21:13:09 +0100

    update training function

--- src/model.py
+++ src/model.py
@@ -1,19 +1,17 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.backend as K
-from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
-from scipy.special import factorial, factorial2
+from scipy.special import factorial
+from tqdm.notebook import tqdm
+from src.utils import *
 
 K.set_floatx('float64')
 
 
-def gaussian_moment(p):
-    if p % 2 == 0:
-        return factorial2(p-1)
-    else:
-        return 0
+
 
 class MomentLoss():
     def __init__(self, degree=4, p_norm=2, weights=None):
@@ -38,51 +36,9 @@ class MomentLoss():
 
 
 
-class GaussianKernel():
-    def __init__(self, length_scale=1.0):
-        self.length_scale = length_scale
-
-    def __call__(self, x_1, x_2):
-        return K.exp( -1/self.length_scale**2 * (x_1 - x_2)**2)
 
 
 
-
-def compute_pairwise_distances(x, y):
-    """Computes the squared pairwise Euclidean distances between x and y.
-    Args:
-    x: a tensor of shape [num_x_samples, num_features]
-    y: a tensor of shape [num_y_samples, num_features]
-    Returns:
-    a distance matrix of dimensions [num_x_samples, num_y_samples].
-    Raises:
-    ValueError: if the inputs do no matched the specified dimensions.
-    """
-
-    if not len(x.get_shape()) == len(y.get_shape()) == 2:
-        raise ValueError('Both inputs should be matrices.')
-
-    if x.get_shape().as_list()[1] != y.get_shape().as_list()[1]:
-        raise ValueError('The number of features should be the same.')
-
-    norm = lambda x: tf.reduce_sum(tf.square(x), 1)
-    return tf.transpose(norm(tf.expand_dims(x, 2) - tf.transpose(y)))
-
-def gaussian_kernel_matrix(x, y, length_scale=1.0):
-    """Computes a Guassian Radial Basis Kernel between the samples of x and y.
-    We create a sum of multiple gaussian kernels each having a width sigma_i.
-    Args:
-    x: a tensor of shape [num_samples, num_features]
-    y: a tensor of shape [num_samples, num_features]
-    sigmas: a tensor of floats which denote the widths of each of the
-    gaussians in the kernel.
-    Returns:
-    A tensor of shape [num_samples{x}, num_samples{y}] with the RBF kernel.
-    """
-    beta = 1. / (2. * length_scale)
-    dist = compute_pairwise_distances(x, y)
-    s = beta * tf.reshape(dist, (1, -1))
-    return tf.reshape(tf.reduce_sum(tf.exp(-s), 0), tf.shape(dist))
 
 class MMDLoss():
     def __init__(self, kernel=gaussian_kernel_matrix, **kwargs):
@@ -114,7 +70,6 @@ def create_train_dataset(x_train, batch_size):
     train_dataset = train_dataset.batch(batch_size)
     return train_dataset
 
-
 class FFNetwork(Model):
     def __init__(self, n_layers, h_dims=64, loss=None):
         super(FFNetwork, self).__init__()
@@ -124,6 +79,7 @@ class FFNetwork(Model):
         layers.append(Dense(1, activation='softplus'))
         self.h_layers = layers
         self.loss = loss
+        self.optimizer = Adam(lr=0.001)
 
     def call(self, x):
         for layer in self.h_layers:
@@ -133,23 +89,40 @@ class FFNetwork(Model):
     def _add_loss(self, loss):
         self.loss = loss
 
-    def train(self, x_train, num_epochs, batch_size, lr):
-        optimizer = Adam(lr=lr)
+    def _set_lr(self, lr):
+        if lr is not None:
+            self.optimizer = Adam(lr=lr)
+
+    def _mse_metric(self, var_true, var_pred):
+        return tf.reduce_mean((var_true - var_pred)**2)
+
+    def train_step(self, x_batch):
+        total_increment = x_batch[:, -1] - x_batch[:, 0]
+        with tf.GradientTape() as tape:
+            int_var = self(x_batch)
+            int_var = tf.squeeze(int_var)
+            loss_value = self.loss(total_increment, int_var)
+        grads = tape.gradient(loss_value, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        return loss_value
+
+    def train(self, x_train, num_epochs, batch_size, lr=None, true_int_var=None, plotting=True):
+        self._set_lr(lr)
+        n_steps = x_train.shape[0] // batch_size
         x_train = create_train_dataset(x_train, batch_size)
-
-        @tf.function
-        def train_step(x_batch):
-            total_increment = x_batch[:,-1] - x_batch[:,0]
-            with tf.GradientTape() as tape:
-                int_var = self.call(x_batch)
-                int_var = K.reshape(int_var, x_batch.shape[0     ])
-                loss_value = self.loss(total_increment, int_var)
-            grads = tape.gradient(loss_value, self.trainable_weights)
-            optimizer.apply_gradients(zip(grads, self.trainable_weights))
-            return loss_value
-
-        for epoch in range(num_epochs):
-            for step, x_batch in enumerate(x_train):
-                train_step(x_batch)
+        losses = []
+        mses = []
+        for epoch in tqdm(range(num_epochs), desc='Training'):
+            with tqdm(total=n_steps, desc=f'Epoch {epoch}') as progress:
+                for step, x_batch in enumerate(x_train):
+                    progress.update()
+                    loss_val = self.train_step(x_batch)
+                    losses.append(loss_val)
+            int_var = self(x_batch).numpy()
+            if true_int_var:
+                mse_val = self._mse_metric(true_int_var, int_var)
+                mses.append(mse_val)
+            if plotting:
+                plot_hist(x_batch, int_var)
 
 
---
+++ src/utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+from scipy.stats import norm
+from scipy.special import factorial2
+import matplotlib.pyplot as plt
+
+def gaussian_moment(p):
+    """Computes the moments of the standard normal distribution. Used
+    when training via the method of moments.
+    Args:
+    p: an integer
+    Returns:
+    the pth moment of the standard Gaussian.
+    """
+    if p % 2 == 0:
+        return factorial2(p-1)
+    else:
+        return 0
+
+def compute_pairwise_distances(x, y):
+    """Computes the squared pairwise Euclidean distances between x and y.
+    Args:
+    x: a tensor of shape [num_x_samples, num_features]
+    y: a tensor of shape [num_y_samples, num_features]
+    Returns:
+    a distance matrix of dimensions [num_x_samples, num_y_samples].
+    Raises:
+    ValueError: if the inputs do no matched the specified dimensions.
+    """
+    if not len(x.get_shape()) == len(y.get_shape()) == 2:
+        raise ValueError('Both inputs should be matrices.')
+
+    if x.get_shape().as_list()[1] != y.get_shape().as_list()[1]:
+        raise ValueError('The number of features should be the same.')
+
+    norm = lambda x: tf.reduce_sum(tf.square(x), 1)
+    return tf.transpose(norm(tf.expand_dims(x, 2) - tf.transpose(y)))
+
+def gaussian_kernel_matrix(x, y, length_scale=1.0):
+    """Computes a Guassian Radial Basis Kernel between the samples of x and y.
+    We create a sum of multiple gaussian kernels each having a width sigma_i.
+    Args:
+    x: a tensor of shape [num_samples, num_features]
+    y: a tensor of shape [num_samples, num_features]
+    sigmas: a tensor of floats which denote the widths of each of the
+    gaussians in the kernel.
+    Returns:
+    A tensor of shape [num_samples{x}, num_samples{y}] with the RBF kernel.
+    """
+    beta = 1. / (2. * length_scale)
+    dist = compute_pairwise_distances(x, y)
+    s = beta * tf.reshape(dist, (1, -1))
+    return tf.reshape(tf.reduce_sum(tf.exp(-s), 0), tf.shape(dist))
+
+def plot_hist(x_batch, int_var):
+
+    total_increment = x_batch[:, -1] - x_batch[:, 0]
+    gen_z = total_increment / np.sqrt(int_var)
+    x = np.linspace(-3.0, 3.0, 100)
+    plt.hist(gen_z)
+    plt.plot(x, norm.pdf(x, 0.0, 1.0))
+    plt.show()
