3f4b66eea836c151460888ae070de2e5769fa10f
phillipmurray <37044444+phillipmurray@users.noreply.github.com>
2021-01-14 11:36:34 +0000

    updated comments on generator

--- src/data_generator.py
+++ src/data_generator.py
@@ -2,8 +2,20 @@ import numpy as np
 import tensorflow as tf
 from src.stoch_vol import ExpOU
 
-class SemiMartingale():
+class SemiMartingale:
+
     def __init__(self, X_0, vol, **kwargs):
+        """
+            Generic class for a semimartingale process.
+
+            Parameters:
+
+            X_0 : float
+                the initial value of the semimartingale process
+            vol : str / np.array
+                the stochastic volatility process driving the semimartingale.
+                Can either be a string or separately generated volatility process.
+        """
         self.X_0 = X_0
         self.vol = vol
         self.seed = None
@@ -13,6 +25,27 @@ class SemiMartingale():
         self.paths = None
 
     def _generate_vol(self, vol, n_paths, total_timesteps, n_timesteps=None, seed=None, **kwargs):
+        """
+            Generate the volatility process over the number of timesteps
+
+            Parameters:
+            -----------
+            vol : float / str / np.array
+                the vol process to generate
+            n_paths : int
+                the number of paths to generate
+            total_timesteps : int
+                the total number of timesteps that the volatility process is generated over for
+                a single day - the frequency of observations
+            n_timesteps : int
+                the number of timesteps to generate at this frequency
+            seed : int
+                random seed to be used
+
+            Returns:
+            -----------
+            numpy array of volatility
+        """
         self.seed = seed
         np.random.seed(seed)
         if n_timesteps is None:
@@ -31,6 +64,25 @@ class SemiMartingale():
 
 
     def generate(self, n_paths, total_timesteps, n_timesteps=None, seed=None, **kwargs):
+        """
+            Generate the semimartingale process
+
+            Parameters:
+            -----------
+            n_paths : int
+                the number of paths to generate
+            total_timesteps : int
+                the total number of timesteps that the volatility process is generated over for
+                a single day - the frequency of observations
+            n_timesteps : int
+                the number of timesteps to generate at this frequency
+            seed : int
+                random seed to be used
+
+            Returns:
+            -----------
+            numpy array of simulated paths
+        """
         self.seed = seed
         np.random.seed(self.seed)
         if n_timesteps is None:
@@ -47,6 +99,19 @@ class SemiMartingale():
         return X
 
     def generate_from_vol(self, seed=None):
+        """
+            Generate the semi martingale from the volatility array
+
+            Parameters:
+            -----------
+            seed: int
+                random seed to be used for Brownian increments
+
+            Returns:
+            -----------
+            numpy array of simulated paths:
+
+        """
         self.seed = seed
         np.random.seed(seed)
         n_paths, n_timesteps = self.vol.shape
@@ -61,33 +126,108 @@ class SemiMartingale():
         return X
 
     def integrated_variance(self, return_mean=False):
+        """
+            Calculate the integrated variance from the paths
+
+            Parameters:
+            -----------
+            return_mean: bool
+                whether to calculate the mean integrated variance over
+                all paths or not
+
+            Returns:
+            -----------
+            numpy array of integrated variance for each path
+        """
         int_var = np.sum(self.vol_paths**2, axis=-1)/self.total_timesteps
         if return_mean:
             int_var = np.mean(int_var, axis=0)
         return int_var
 
     def get_increments(self):
+        """
+            Calculate the increments of the semimartingale process: X_t - X_{t-1}
+
+            Returns:
+            -----------
+            numpy array of the increments
+        """
         if self.paths is not None:
             return np.diff(self.paths, axis=-1)
 
     def reshape_for_rnn(self):
+        """
+            Reshape the paths into the correct shape for a tensorflow RNN
+
+            Returns:
+            -----------
+            numpy array of shape (paths, timesteps, features)
+        """
         if self.paths is not None:
             return np.reshape(self.paths, (self.n_paths, self.n_timesteps+1, 1))
 
     def get_total_increments(self):
+        """
+            Calculate the total increment of the path timeline: X_1 - X_0
+
+            Returns:
+            -----------
+            numpy array of the total increment for each path
+        """
         if self.paths is not None:
             return self.paths[:,-1] - self.paths[:,0]
 
     def get_norm_increments(self):
+        """
+            Calculate the total increment normalised by the integrated variance:
+            (X_1 - X_0) / sqrt(IV)
+
+            Returns:
+            -----------
+            numpy array of normalised increments
+        """
         if self.paths is not None:
             increments = self.get_total_increments()
             integrated_var = self.integrated_variance()
             return increments / np.sqrt(integrated_var)
 
-    def _to_tensor(self):
-        return tf.constant(self.paths)
+    def _to_tensor(self, for_rnn=False):
+        """
+            Convert the numpy array paths to a tensorflow tensor
+
+            Parameters:
+            -----------
+            for_rnn : bool
+                Whether to reshape the paths for RNN
+
+            Returns:
+            -----------
+            tensorflow tensor
+        """
+        if self.paths:
+            if for_rnn:
+                paths = self.reshape_for_rnn()
+            else:
+                paths = self.paths
+            return tf.constant(paths)
 
     def add_noise(self, sd=0.5, seed=None):
+        """
+            Add Gaussian noise to the paths
+            TODO: Change distribution of the noise - Gaussian is not a good
+            model for market microstructure noise
+
+            Parameters:
+            -----------
+            sd : float
+                Standard deviation of the noise
+            seed : int
+                random seed to use
+
+            Returns:
+            -----------
+            numpy array of the paths with added noise
+        """
         self.seed = seed
         np.random.seed(seed)
         if self.paths is not None:
