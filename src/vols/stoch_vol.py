2d0b3268754d3f79d8eb6db08300a58e2ce52b50
Alessandro Micheli <am1118@ic.ac.uk>
2021-01-18 21:25:58 +0000

    moved stoch_vols

---
+++ src/vols/stoch_vol.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class ExpOU():
+    def __init__(self, theta=1.0, beta=0.2):
+        self.beta = beta
+        self.theta = theta
+
+    def generate(self, n_paths, total_timesteps, n_timesteps=None, seed=None):
+        self.seed=seed
+        np.random.seed(seed)
+        if n_timesteps is None:
+            n_timesteps = total_timesteps
+        dt = np.sqrt(1/total_timesteps)
+        log_vol = np.zeros((n_paths, n_timesteps))
+        log_vol[:,0] = np.random.normal(size=n_paths, loc=0.0, scale=np.sqrt(1/(2*self.theta)))
+        for t in range(1, n_timesteps):
+            log_vol[:,t] = log_vol[:,t-1] * (1 - self.theta * dt) + np.random.normal(size=n_paths, loc=0.0,  scale=np.sqrt(dt))
+        return np.exp(log_vol)
