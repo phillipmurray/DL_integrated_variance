87bb0da0cd678c4accb1d29a0024d152f7be81f2
Alessandro Micheli <am1118@ic.ac.uk>
2021-01-21 17:10:27 +0000

    added wasserstein loss

--- src/losses/wasserstein.py
+++ src/losses/wasserstein.py
@@ -1,10 +1,23 @@
 import numpy as np
+import tensorflow as tf
 
 
-class WassersteinLoss:
-    def __init__(self, mean = 0, std = 1):
+class Wasserstein2Loss:
+    """2-Wasserstein distance  between 2 Normal Distributions
+    Remark: 2-Wasserstein distance is an upper bound of 
+    1-Wasserstein distance.
+    """
+    def __init__(self, mean = 0, variance = 1):
         self.mean = mean
-        self.std = std
+        self.variance = variance
         
-    def __call__(self, prediction,  ):
-
+    def __call__(self, total_increments, integrated_var):
+        gen_sample = total_increments / tf.cast(tf.math.sqrt(integrated_var),tf.float32)
+        gen_sample = tf.reshape(gen_sample, (gen_sample.shape[0], 1))
+        sample_mean = tf.reduce_mean(gen_sample)
+        sample_variance = tf.math.reduce_std(gen_sample)**2
+        cost = tf.math.abs(sample_mean -self.mean)
+        cost += self.variance + sample_variance 
+        cost -= 2*tf.math.sqrt(sample_variance*self.variance)
+        print(cost)
+        return cost
