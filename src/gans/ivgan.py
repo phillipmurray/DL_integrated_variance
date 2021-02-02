dc714222f34b5f5867a68090295b8e43020ebc55
Alessandro Micheli <am1118@ic.ac.uk>
2021-02-02 09:28:27 +0000

    bug fixes

--- src/gans/ivgan.py
+++ src/gans/ivgan.py
@@ -21,11 +21,13 @@ class IVGan:
     def train_critic(self, true_batch, fake_batch, clip):
         fake_iv = tf.squeeze(self.generator(fake_batch))
         fake_iv = np.abs(np.where(fake_iv ==0 , 0.000001, fake_iv))
-        fake_sample = (fake_batch[:,-1,:] - fake_batch[:,0,:])/np.sqrt(fake_iv)
+        fake_sample = (fake_batch[:,-1] - fake_batch[:,0])/np.sqrt(fake_iv)
         fake_sample = np.reshape(fake_sample,(fake_sample.shape[0],1))
         true_sample = np.reshape(true_batch,(true_batch.shape[0],1))
         with tf.GradientTape() as tape:
             #we want to maximise so we need to put "-"
+            mean_fake = tf.reduce_mean(self.critic(fake_sample))
+            mean_true = tf.reduce_mean(self.critic(true_sample))
             critic_loss = tf.reduce_mean(self.critic(fake_sample) -self.critic(true_sample))
         grads = tape.gradient(critic_loss, self.critic.trainable_weights)
         self.critic.optimizer.apply_gradients(zip(grads, self.critic.trainable_weights))
@@ -36,7 +38,7 @@ class IVGan:
         with tf.GradientTape() as tape:
             iv = tf.cast(tf.squeeze(self.generator(x_batch)),tf.float32)
             iv = tf.math.abs(tf.where(iv ==0 , 0.000001, iv))
-            gen_z = (x_batch[:,-1,:] - x_batch[:,0,:])/tf.math.sqrt(iv)
+            gen_z = (x_batch[:,-1] - x_batch[:,0])/tf.math.sqrt(iv)
             gen_z = tf.reshape(gen_z,(gen_z.shape[0],1))
             generator_loss = -tf.reduce_mean(self.critic(gen_z))
         grads = tape.gradient(generator_loss, self.generator.trainable_weights)
@@ -47,8 +49,8 @@ class IVGan:
         self._set_lr(lr)
         sample_size = x_train.shape[0] 
         n_steps = sample_size // batch_size
-        x_fake = create_train_dataset(x_train, batch_size).repeat().__iter__()
-        x_true = create_train_dataset(true_sample, batch_size).repeat().__iter__()
+        x_fake = create_train_dataset(x_train, batch_size,drop_remainder = True).repeat().__iter__()
+        x_true = create_train_dataset(true_sample, batch_size,drop_remainder = True).repeat().__iter__()
         critic_losses = []
         generator_losses = []
         for epoch in range(num_epochs):
