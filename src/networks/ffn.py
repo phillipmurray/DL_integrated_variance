ca33b76573936c2071d16ded096360e36f42e073
Alessandro Micheli <am1118@ic.ac.uk>
2021-02-04 11:12:59 +0000

    added batch normalization to ffn

--- src/networks/ffn.py
+++ src/networks/ffn.py
@@ -4,12 +4,18 @@ from tensorflow.keras.optimizers import Adam
 from src.networks.ivmodel import IVModel
 
 class FFNetwork(IVModel):
-    def __init__(self, n_layers, h_dims=64, loss=None, **kwargs):
+    def __init__(self, n_layers, h_dims=64, loss=None, batch_norm = False, **kwargs):
         super().__init__()
         layers = []
         for _ in range(n_layers):
+            if batch_norm:
+                layers.append(BatchNormalization())
             layers.append(Dense(h_dims, activation='relu'))
+        if batch_norm:
+                layers.append(BatchNormalization())
         layers.append(Dense(h_dims, activation=None))
+        if batch_norm:
+                layers.append(BatchNormalization())
         layers.append(Dense(1, activation='softplus'))
         self.h_layers = layers
         self.loss = loss
